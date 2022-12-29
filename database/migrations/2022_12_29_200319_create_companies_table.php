657d3995776661fff272cf5186afc2615b9c7953
Wesllycode <wesllycode07@gmail.com>
2022-12-29 17:32:14 -0300

    fix: renomei os nomes Image para Company

--- database/migrations/2022_12_29_200319_create_companies_table.php
+++ database/migrations/2022_12_29_200319_create_companies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('asn');
