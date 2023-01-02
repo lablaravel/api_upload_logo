903c1274798737c5d19a36c2ef6cf9991b27aea2
Wesllycode <wesllycode07@gmail.com>
2023-01-02 15:23:31 -0300

    feat: funcionando upload de imagem

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateCompanyRequest;
+use App\Models\Company;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 
 class CompanyController extends Controller
 {
-    public function createCompany(CreateCompanyRequest $request){
-        $validated = $request->validated();    
-        return 'oi';
+    public function createCompany(CreateCompanyRequest $request): jsonResponse {
+        
+        /**
+         * Não preciso usar validated->fails(), pois não aceita nesses formato, a própria class createCompanyRequest
+         * Vai se responsabilizar para verificar os erros e retornar um resultado de sucesso ou false.
+         */
+
+        $validated = $request->validated();        
+
+        $validated['url_logo'] = $request->file('url_logo')->store('images/logo');
+        $data = Company::create($validated);
+        return response()->json(array($data, Response::HTTP_CREATED));
     }
 }
--- app/Http/Requests/CreateCompanyRequest.php
+++ app/Http/Requests/CreateCompanyRequest.php
@@ -21,12 +21,31 @@ class CreateCompanyRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules():  mixed
     {
         return [
-                'name' => 'required|max:100',
-                'asn' => 'required|max:100',
+                'name' => 'required|min:2|max:100',
+                'asn' => 'required|min:3|max:50',
                 'url_logo' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
             ];
+    }
+
+    /** 
+     * Get he errors messagens personalized
+     * 
+     * @return array
+    */   
+    public function messages()
+    {
+        return [
+            'name.required' => 'Digite um nome da empresa válido',
+            'name.min' => 'Digite um nome com no mínimo de 2 caracteres',
+            'name.max' => 'Permitido um nome apenas de 50 caracteres',
+            'asn.required' => 'Digite um ASN Válido',
+            'asn.min' => 'Digite um asn com no minimo 3 caracteres.',
+            'asn.max' => 'Digite um asn com no máximo 50 caracteres',
+            'url_logo.required' =>'Selecione uma imagem',
+            'url_logo.mimes' => 'Apenas esse formato válido o png'
+        ];
     }
 }
