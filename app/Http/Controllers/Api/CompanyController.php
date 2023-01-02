d0a0b7895bcda8b368369e81ecc53de3bfb2a1c4
wesllycode <wesllycode07@gmail.com>
2023-01-02 12:52:38 -0300

    wip

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCompanyRequest;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    public function createCompany(){
-
+    public function createCompany(CreateCompanyRequest $request){
+        $validated = $request->validated();
     }
 }
